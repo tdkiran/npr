bebf4248c559ea53b5e82fb14921ba673a1aa7df
Kiran Thirumalaichery Dhakshinamoorthy <kirandhakshina@gmail.com>
2021-02-02 22:03:23 -0600

    fix issue

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,22 @@ import { spawn } from "child_process";
 
 const args = process.argv.slice(2);
 
-spawn('npm', ['run', ...args ]);
+if (args && args.length) {
+    const ls = spawn('npm', ['run', ...args]);
+
+    ls.stdout.on('data', (data) => {
+        console.log(`stdout: ${data}`);
+    });
+
+    ls.stderr.on('data', (data) => {
+        console.error(`stderr: ${data}`);
+    });
+
+    ls.on('close', (code) => {
+        console.log(`child process exited with code ${code}`);
+    });
+}
+
 
 // const output = arguments.reduce((output, currentArg) => {
 //     output = output + ' ' + currentArg;
